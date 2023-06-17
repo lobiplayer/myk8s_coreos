f3223676f9c02fa2cd1f36003909dcdf285cb4e6
Robin Mohan <jrobinmohan@gmail.com>
2023-06-17 15:20:50 +0200

    new codes

---
+++ CoreOS/masternode1/yaml_to_ign.sh
@@ -0,0 +1,2 @@
+
+podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < k3s-server.yaml > k3s-server.ign
---
+++ CoreOS/workernode1/yaml_to_ign.sh
@@ -0,0 +1,2 @@
+
+podman run -i --rm quay.io/coreos/fcct:release --pretty --strict < k3s-client1.yaml > k3s-client1.ign
